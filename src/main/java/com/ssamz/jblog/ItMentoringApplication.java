c2d73360cbd3aa58962a2294bbb8031a820c1e3c
knecht <ldj910505@naver.com>
2023-06-03 22:40:04 +0900

    initialize repository

---
+++ src/main/java/com/ssamz/jblog/ItMentoringApplication.java
@@ -0,0 +1,13 @@
+package com.ssamz.jblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItMentoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItMentoringApplication.class, args);
+	}
+
+}
