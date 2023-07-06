b8ce00868f6db7087e18caebaad397813c9e5ae8
knecht <ldj910505@naver.com>
2023-07-06 16:15:00 +0900

    예외를 적용하여 작성

--- src/main/java/com/ssamz/jblog/controller/UserController.java
+++ src/main/java/com/ssamz/jblog/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.ssamz.jblog.controller;
 
+import java.util.function.Supplier;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssamz.jblog.entity.UserEntity;
+import com.ssamz.jblog.exception.JBlogException;
 import com.ssamz.jblog.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,7 +26,12 @@ public class UserController {
 	@GetMapping("/user/get/{id}")
 	public @ResponseBody UserEntity getUser(@PathVariable String id) {
 		// 특정 id 에 해당하는 User 객체 리턴
-		UserEntity findUser = userService.findById(id).get();
+		UserEntity findUser = userService.findById(id).orElseThrow(new Supplier<JBlogException>() {
+			@Override
+			public JBlogException get() {
+				return new JBlogException(id + "를 가진 회원이 존재하지 않습니다");
+			}
+		});
 		return findUser;
 	}
 
