206517a4003d63871cc8569d3fe9a5444f59054b
knecht <ldj910505@naver.com>
2023-07-07 18:00:49 +0900

    예외 처리 변경

--- src/main/java/com/ssamz/jblog/exception/JBlogExceptionHandler.java
+++ src/main/java/com/ssamz/jblog/exception/JBlogExceptionHandler.java
@@ -1,16 +1,19 @@
 package com.ssamz.jblog.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ssamz.jblog.dto.ResponseDTO;
 
 @ControllerAdvice
 @RestController
 public class JBlogExceptionHandler {
 
 	@ExceptionHandler(value = Exception.class)
-	public String globalExceptionHandler(Exception e) {
-		return "<h1>" + e.getMessage() + "</h1>";
+	public ResponseDTO<String> globalExceptionHandler(Exception e) {
+		return new ResponseDTO<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
 	}
 	
 }
