231f86bb2209a2bbbe54684eaf43f86da2b584e6
knecht <ldj910505@naver.com>
2023-07-15 22:05:15 +0900

    postcontroller 작성

---
+++ src/main/java/com/ssamz/jblog/controller/PostController.java
@@ -0,0 +1,17 @@
+package com.ssamz.jblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class PostController {
+
+	@GetMapping("/post/insertPost")
+	public String insertPost() {
+		return "post/insertPost";
+	}
+	
+}
